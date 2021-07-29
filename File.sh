4d3f41eddf1f6829a8aaee7f33a26f05327d1fce
Ewelina <ewelinakolba00@gmail.com>
2021-07-29 20:36:36 +0200

    right

--- File.sh
+++ File.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-e="$(sudo docker container ls -a)"
+c="$(nproc)"
+echo "Ilość rdzeni procesora: $c"
+d="$(free -g)"
+echo "Ilość pamięci w GB: "
+echo "$d"
+e="$(docker container ls -a)"
 echo "Kontenery Docker'a dostępne na hoście: $e"
-f="$(sudo docker ps-a)"
-echo "Kontenery Docker'a dostępne na hoście: $f"
-
--- addItem.sh
+++ addItem.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "ala"
+e="$(sudo docker container ls -a)"
+echo "Kontenery Docker'a dostępne na hoście: $e"
+f="$(sudo docker ps-a)"
+echo "Kontenery Docker'a dostępne na hoście: $f"
+
