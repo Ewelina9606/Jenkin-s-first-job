5322cae9d6153b9b9750558617c121abc7727a28
Ewelina <ewelinakolba00@gmail.com>
2021-07-29 20:31:17 +0200

    m

--- File.sh
+++ File.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-c="$(nproc)"
-echo "Ilość rdzeni procesora: $c"
-d="$(free -g)"
-echo "Ilość pamięci w GB: "
-echo "$d"
-e="$(docker container ls -a)"
+e="$(sudo docker container ls -a)"
 echo "Kontenery Docker'a dostępne na hoście: $e"
+f="$(sudo docker ps-a)"
+echo "Kontenery Docker'a dostępne na hoście: $f"
+
