e2b5439074e919c6631b9fffd9257be0d7aac3d0
Ewelina <ewelinakolba00@gmail.com>
2021-07-29 20:23:10 +0200

    n

--- addItem.sh
+++ addItem.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-e="$(sudo docker container ls -a)"
-echo "Kontenery Docker'a dostępne na hoście: $e"
-f="$(sudo docker ps -a)"
-echo "Kontenery Docker'a dostępne na hoście: $f"
+echo "ala"
